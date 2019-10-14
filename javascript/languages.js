965443840d03d8572d7fec717e4b519d26629899
Nguyendanghung0201 <danghung020195@gmail.com>
2019-10-14 10:08:56 +0700

    commit video3

--- javascript/languages.js
+++ javascript/languages.js
@@ -510,8 +510,8 @@ function body(db) {
                                                 allowfullscreen></iframe> -->
                                             <video id="video-trade" muted="false" loop>
                                                 <source src="images/video/buy-apk.mp4" type = "video/mp4">
-                                                <source src="images/video/sell-apk.ogg" type = "video/mp4">
-                                                <source src="images/video/sell-apk.webm" type = "video/mp4">
+                                                <source src="images/video/sell-apk.ogg" type = "video/ogg">
+                                                <source src="images/video/sell-apk.webm" type = "video/webm">
 
                                                 This browser doesn't support video tag.
                                             </video>
